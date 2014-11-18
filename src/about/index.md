a43e5c7d3a688d5fb009786a628aa43406b61356
Florian Poulin <florian.poulin@gmail.com>
2014-11-18 13:10:49 +0800

    Some layout for the website

---
+++ src/about/index.md
@@ -0,0 +1,8 @@
+---
+title: About
+sectionName: About
+template: layout.jade
+menuIndex: 4
+---
+
+More about this project later, when I'll be sure it's going somewhere :)
--- static/js/app.js
+++ static/js/app.js
@@ -5,3 +5,17 @@ window.addEventListener("hashchange", shiftWindow);
 function load() {
 	if (window.location.hash) shiftWindow();
 }
+
+$(window).load(function() {
+  var icons = ["adjust", "asterisk", "bell", "briefcase","camera", 
+ 			"cloud", "cog", "cutlery", "earphone", "envelope", "eye-open", 
+ 			"facetime-video", "fire", "flag", "flash", "gift", "glass", 
+ 			"headphones", "heart", "home", "music", "paperclip", "pencil",
+ 			"phone", "phone-alt", "plane", "pushpin", "shopping-cart", "star",
+ 			"time", "tower", "tree-conifer", "tree-deciduous", "user", "wrench"];
+
+  $("div.header > div.container > h1 > span.glyphicon.rand").each(function() {
+  	$(this).attr("class", "glyphicon glyphicon-" + icons[Math.floor(Math.random()*icons.length)]);
+  })
+});
+
